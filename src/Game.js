7b67662ceb1bf870f6e0474bf2c31eb70363de70
Azeezah Muhammad <azeezahm14@gmail.com>
2020-01-30 01:56:54 -0800

    Show guesses

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,12 @@
-import React, {Component,} from 'react';
+import React, {Component} from 'react';
 import Board from './Board';
 
 class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      guess: ''
+      guess: '',
+      guesses: ["cat", "dogs"]
     };
   }
   changeGuess = (e) => {
@@ -13,20 +14,27 @@ class Game extends Component {
   }
   submitGuess = (e) => {
     e.preventDefault();
+    const guesses = this.state.guesses
+    this.setState({guesses:([...guesses, this.state.guess])})
     this.setState({guess: ''})
   }
 
   render() {
+    const guesses = this.state.guesses.map((item, key)=>
+      <span>{item}<br /></span>
+    );
+
     return (
       <div>
         <Board />
-        <form>
+        <form autocomplete="off">
           <input
             name='guess'
             value={this.state.guess}
             onChange={(e) => this.changeGuess(e)} />
           <button hidden onClick={(e) => this.submitGuess(e)}>Guess</button>
         </form>
+        {guesses}
       </div>
     )
   }
