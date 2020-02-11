c62e424f2012289dcd9293a821b3b20046ad99b8
Azeezah Muhammad <azeezahm14@gmail.com>
2020-02-10 20:22:38 -0800

    Add login button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Game from './Game';
+import LoginButton from './LoginButton.js';
+import firebase from 'firebase';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {playing:false}
+    this.state = {playing:false, user:null}
   }
   startPlaying = () => {this.setState({playing:true})}
   stopPlaying = () => {this.setState({playing:false})}
@@ -14,10 +16,13 @@ class App extends Component {
   render() {
     const startButton = (<button onClick={this.startPlaying}>Start</button>)
     const stopButton = (<button onClick={this.stopPlaying}>Home</button>)
+    const user = this.state.user;
     return (
       <div class="app">
       <center>
         <h1 class="appTitle">BOGGLE</h1>
+        <LoginButton setUser={(user)=>this.setState({user:user})} />
+        {user != null && <p>Welcome, {user.displayName} ({user.email})</p>}
         { this.state.playing ? stopButton : startButton }
         { this.state.playing ? <Game /> : null }
       </center>
